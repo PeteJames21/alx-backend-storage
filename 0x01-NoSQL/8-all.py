e7797eb33db8c462d7162f72e3496eca1b4a4f99
PeteJames21 <peetjames21@gmail.com>
2024-03-26 20:29:06 +0300

    Remove annotation

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 """Defines a function that lists all documents in a collection."""
 
-from pymongo.collection import Collection
 
-
-def list_all(mongo_collection: Collection):
+def list_all(mongo_collection):
     """List all documents in the collection."""
     return list(mongo_collection.find({}))
