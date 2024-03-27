834c65b3b37eedba716a29e53fe45f182850cf20
PeteJames21 <peetjames21@gmail.com>
2024-03-27 22:30:06 +0300

    Implement methods for retrieving data

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 """
+Defines a Cache class that uses Redis to implement cache functionality.
+Any data stored in a previous session is cleared when the next session
+begins.
 """
+
 import redis
-from typing import Union
+from typing import Optional, Union
 from uuid import uuid4
 
 
@@ -16,3 +20,25 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[callable] = None) -> Union[str, bytes, int, float]:
+        """Retrieve and object from storage in its original format."""
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """Retrieve a string from storage."""
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """Retrieve an int from storage."""
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode("utf-8"))
+        except Exception:
+            value = 0
+        return value
